3ec61fb6c029a12af607d0afd5f9d49e3b37775a
xxu-02 <xxu02@ucm.es>
2026-03-24 21:16:13 +0100

    Dash del boss

---
+++ Assets/Scripts/BossDash.cs
@@ -0,0 +1,109 @@
+//---------------------------------------------------------
+// Breve descripción del contenido del archivo
+// Responsable de la creación de este archivo
+// Nombre del juego
+// Proyectos 1 - Curso 2025-26
+//---------------------------------------------------------
+
+using System.Collections;
+using UnityEngine;
+using UnityEngine.Rendering;
+// Añadir aquí el resto de directivas using
+
+
+/// <summary>
+/// Antes de cada class, descripción de qué es y para qué sirve,
+/// usando todas las líneas que sean necesarias.
+/// </summary>
+public class BossDash : MonoBehaviour
+{
+    // ---- ATRIBUTOS DEL INSPECTOR ----
+    #region Atributos del Inspector (serialized fields)
+    // Documentar cada atributo que aparece aquí.
+    // El convenio de nombres de Unity recomienda que los atributos
+    // públicos y de inspector se nombren en formato PascalCase
+    // (palabras con primera letra mayúscula, incluida la primera letra)
+    // Ejemplo: MaxHealthPoints
+    [SerializeField] private float DashPower = 8f; // fuerza o velocidad del dash
+    [SerializeField] private float DashTime = 0.3f; // duracion del dash
+    [SerializeField] private float DashColdDown = 3f; // duracion entre cada dash 
+
+    #endregion
+
+    // ---- ATRIBUTOS PRIVADOS ----
+    #region Atributos Privados (private fields)
+    // Documentar cada atributo que aparece aquí.
+    // El convenio de nombres de Unity recomienda que los atributos
+    // privados se nombren en formato _camelCase (comienza con _, 
+    // primera palabra en minúsculas y el resto con la 
+    // primera letra en mayúsculas)
+    // Ejemplo: _maxHealthPoints
+    private float Timer; // tiempo que empieza desde 0
+    private Rigidbody2D rb;
+    private bool CanDash = true;
+    #endregion
+
+    // ---- MÉTODOS DE MONOBEHAVIOUR ----
+    #region Métodos de MonoBehaviour
+
+    // Por defecto están los típicos (Update y Start) pero:
+    // - Hay que añadir todos los que sean necesarios
+    // - Hay que borrar los que no se usen 
+
+    /// <summary>
+    /// Start is called on the frame when a script is enabled just before 
+    /// any of the Update methods are called the first time.
+    /// </summary>
+    void Start()
+    {
+        Timer = 0;
+        rb = GetComponent<Rigidbody2D>();
+        StartCoroutine(Dash());
+    }
+
+    /// <summary>
+    /// Update is called every frame, if the MonoBehaviour is enabled.
+    /// </summary>
+    void Update()
+    {
+    }
+    #endregion
+
+    // ---- MÉTODOS PÚBLICOS ----
+    #region Métodos públicos
+    // Documentar cada método que aparece aquí con ///<summary>
+    // El convenio de nombres de Unity recomienda que estos métodos
+    // se nombren en formato PascalCase (palabras con primera letra
+    // mayúscula, incluida la primera letra)
+    // Ejemplo: GetPlayerController
+
+    #endregion
+
+    // ---- MÉTODOS PRIVADOS ----
+    #region Métodos Privados
+    // Documentar cada método que aparece aquí
+    // El convenio de nombres de Unity recomienda que estos métodos
+    // se nombren en formato PascalCase (palabras con primera letra
+    // mayúscula, incluida la primera letra)
+    private IEnumerator Dash()
+    {
+        while(CanDash)
+        {
+            float originalGScale = rb.gravityScale;
+            yield return new WaitForSeconds(DashColdDown);
+        
+            Timer = 0;
+            while(Timer<DashTime)
+            {   
+                rb.gravityScale = 0;
+                rb.linearVelocity = new Vector2(transform.localScale.x * DashPower, 0f);
+                Timer += Time.deltaTime;
+                yield return null;
+            }
+            rb.gravityScale = originalGScale;
+        }
+    }
+    #endregion   
+
+} // class BossDash 
+// namespace
